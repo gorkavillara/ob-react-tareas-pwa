1ac0afc8b92dfbe223c8018353b5499a5088b7e8
Gorka <gorkavillara@gmail.com>
2021-11-12 14:05:17 +0100

    App funcionando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todolist, setTodoList] = useState([]);
+
+  const recordTodo = () => {
+    setTodoList([...todolist, todo]);
+    setTodo("");
+  }
+
+  const Todo = ({ text }) => {
+    const [done, setDone] = useState(false);
+    return (
+      <label className="todo">
+        <input type="checkbox" checked={done} onChange={() => setDone(!done)} className="todo-check" />
+        <span className="todo-text" style={done ? { textDecoration: 'line-through' } : {}}>{text}</span>
+      </label>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tareas con React</h1>
+      <div className="input-todo">
+        <input className="input" type="text" value={todo} onChange={e => setTodo(e.target.value)} onKeyPress={e => (e.key === 'Enter') && recordTodo()} placeholder="Introduce la tarea" />
+        <button className="submit" onClick={recordTodo}>Enviar</button>
+      </div>
+      <div className="todo-header">
+        <span className="todo-header-text">Lista de tareas</span>
+      </div>
+      <div className="todo-list">
+        {todolist.map((todo, k) => <Todo text={todo} key={k} />)}
+      </div>
     </div>
   );
 }
